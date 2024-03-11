214e52cfbcc75af7ed40b9563a017ac858f9cb8f
João Miguel <128449074+JoaoMiguelb@users.noreply.github.com>
2024-03-11 18:19:06 -0300

    Adição de projeto "POSTS".

--- projeto/src/pages/projects/index.jsx
+++ projeto/src/pages/projects/index.jsx
@@ -1,5 +1,6 @@
 // img
 import banco from '../../img/projeto-banco.png'
+import posts from '../../img/posts.png'
 
 // CSS
 import './styles.css'
@@ -7,11 +8,21 @@ import './styles.css'
 const Projects = () => {
   return (
     <div className='projects'>
-      <span>
+     <div className='box'>
+     <span>
         <h3>BANCO JM</h3>
         <p>Site de banco 100% original e responsivo desenvolvido em React JS por mim.</p>
       </span>
       <a href="https://banco-jm.vercel.app" target='_blank'><img src={banco} alt="Monitor e celular com o projeto aberto" /></a>
+     </div>
+
+    <div className='box' >
+    <span>
+        <h3>POSTS</h3>
+        <p>Rede social desenvolvida com React Js e trabalhada com API do unsplash.</p>
+      </span>
+      <a href="https://posts-joaomiguelb.vercel.app" target='_blank'><img src={posts} alt="Monitor e celular com o projeto aberto" /></a>
+    </div>
     </div>
   )
 }
